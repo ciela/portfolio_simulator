14a2ecff878ad29f0f29f50025042d5bc083a95f
ciela <zektbach@gmail.com>
2017-03-20 18:14:36 +0900

    Use histogram of matplotlib

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import sys
 import numpy as np
 import numpy.random as rnd
+import matplotlib.pyplot as plt
 
 
 A = 0.7
 SEED = 10000
+TRIAL = 100
 
 
 def calc_assets(portfolio, days):
@@ -19,10 +21,13 @@ def calc_assets(portfolio, days):
 def main():
     portfolio, days = float(sys.argv[1]), int(sys.argv[2])
     log10_results = []
-    for _ in range(1000):
+    for _ in range(TRIAL):
         assets = calc_assets(portfolio, days)
-        log10_results.append(np.log10(assets / SEED))
-    print(np.histogram(log10_results))
+        log10_results.append(np.floor(np.log10(assets / SEED)).astype('int'))
+    log10_hist, log10_bin, _ = plt.hist(log10_results)
+    print(log10_hist)
+    print(log10_bin)
+    plt.show()
 
 
 main()
